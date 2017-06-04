cc3424abd620fb839f8b22e1befdcf5e5aa1726f
ronscoder <ronsair@Ronjans-MacBook-Air.local>
2017-06-04 16:37:50 +0530

    v1.0.1

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,3 +1,6 @@
-$("document").ready(function(){
-
+//close .dropdown-menu on link click
+$(document).ready(() => {
+    $('ul.dropdown-menu li a, li.dropdown a').click(()=>{
+        $('.dropdown-menu').css('display:none');
+    })
 })
